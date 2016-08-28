2a4f1b319068be484ef265596f2f4e3d523d2e13
Miles McBain <miles.mcbain@gmail.com>
2016-08-29 00:54:49 +1000

    Fix 'conext' typo and tweak pipe from 0 indent behaviour.

--- R/pipeNewlineIndent.R
+++ R/pipeNewlineIndent.R
@@ -7,21 +7,22 @@ pipeNewlineIndent <- function(){
 
   context <- rstudioapi::getActiveDocumentContext()
   context_row <- context$selection[[1]]$range$end["row"]
-  indent_conext <- regexec(pattern = "\\w", context$contents[context_row])[[1]][1]-1 #match pos of the first word -1
-  if(indent_conext < 0 )indent_conext <- length(context$contents[context_row])
+  indent_context <- regexec(pattern = "\\w", context$contents[context_row])[[1]][1]-1 #match pos of the first word -1
+  if(indent_context < 0 )indent_context <- nchar(context$contents[context_row]) #we found only whitespace, stay at this level.
+  if(indent_context == 0)indent_context <- 2 #we are at 0 indent, this is the only time we want to add indent with %>%
 
   if(sum(context$selection[[1]]$range$end -
          context$selection[[1]]$range$start) > 0){
     #Non-empy selection. Just insert the pipe and newline.
-    rstudioapi::insertText(paste0("%>%\n", strrep(" ",indent_conext)))
+    rstudioapi::insertText(paste0("%>%\n", strrep(" ",indent_context)))
   }else{
     if(grepl(pattern = '\\s$', context$contents[context_row])){
       #Empty selection. If the last char of the line is a space do not insert one.
-      rstudioapi::insertText(paste0("%>%\n", strrep(" ",indent_conext)))
+      rstudioapi::insertText(paste0("%>%\n", strrep(" ",indent_context)))
     }
     else{
       #Insert a space
-      rstudioapi::insertText(paste0(" %>%\n", strrep(" ",indent_conext)))
+      rstudioapi::insertText(paste0(" %>%\n", strrep(" ",indent_context)))
     }
   }
 }
