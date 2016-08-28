66b0bca11dcc8d1c98dba506d04be85f073329a6
Miles McBain <miles.mcbain@gmail.com>
2016-08-28 23:17:51 +1000

    Working for pipe and assgin. Some doco errors.

---
+++ R/pipeNewlineIndent.R
@@ -0,0 +1,27 @@
+#' Insert ' %<% newline'.
+#'
+#' Call this function as an addin to insert a pipe and newline at the cursor position.
+#' @export
+#'
+pipeNewlineIndent <- function(){
+
+  context <- rstudioapi::getActiveDocumentContext()
+  context_row <- context$selection[[1]]$range$end["row"]
+  indent_conext <- regexec(pattern = "\\w", context$contents[context_row])[[1]][1]-1 #match pos of the first word -1
+  if(indent_conext < 0 )indent_conext <- length(context$contents[context_row])
+
+  if(sum(context$selection[[1]]$range$end -
+         context$selection[[1]]$range$start) > 0){
+    #Non-empy selection. Just insert the assign and newline.
+    rstudioapi::insertText(paste0("%>%\n", strrep(" ",indent_conext)))
+  }else{
+    if(grepl(pattern = '\\s$', context$contents[context_row])){
+      #Empty selection. If the last char of the line is a space do not insert one.
+      rstudioapi::insertText(paste0("%>%\n", strrep(" ",indent_conext)))
+    }
+    else{
+      #Insert a space
+      rstudioapi::insertText(paste0(" %>%\n", strrep(" ",indent_conext)))
+    }
+  }
+}
